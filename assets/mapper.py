2d1eea207d28dd53f6a4d0a9c277c138767311b3
yasseralami <y.alami@um5s.net.ma>
2026-05-15 01:42:08 +0100

    mr.pys added

---
+++ assets/mapper.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*
+
+import sys
+#Word Count Example
+# input comes from standard input STDIN
+#if sys.argv[1:]: 
+#    arg = sys.argv[1]
+#arg = sys.argv[1]
+for line in sys.stdin:
+    line = line.strip() #remove leading and trailing whitespaces
+    words = line.split() #split the line into words and returns as a list
+    for word in words:
+        #write the results to standard output STDOUT
+        print(f"{word}\t{1}") #print results
